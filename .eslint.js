1830676b93f034ef2cd984ff953b04263cc6ae47
David Chambers <dc@davidchambers.me>
2020-08-15 15:16:30 +0200

    sanctuary-scripts@3.0.x

---
+++ .eslint.js
@@ -0,0 +1,3 @@
+//  This file exists for ESLint to find when matching '*.{js,mjs}'.
+//  ESLint considers it an error to specify a pattern that matches no files
+//  (even if multiple patterns are provided and other patterns do match files).
